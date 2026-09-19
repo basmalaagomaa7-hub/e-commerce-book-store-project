c439a558c73c0c2b253d2181cb9663bb5bfb30f5
sahar <saharelmahdi2@gmail.com>
2026-09-19 03:46:06 +0300

    Added homepage files

---
+++ main.js
@@ -0,0 +1,18 @@
+//LocalStorage
+document.addEventListener("DOMContentLoaded", () => {
+    updateCartCount();
+});
+
+function updateCartCount() {
+    const cartCountElement = document.getElementById("cartCount");
+    if (!cartCountElement) return;
+
+   
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    
+    const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+
+   
+    cartCountElement.textContent = totalItems;
+}
